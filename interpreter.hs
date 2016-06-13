af5e8db3250e652fd5324619a9155a65ccd9a38c
Wonyoung <wonyoung.jang@lge.com>
2016-06-14 08:56:28 +0900

    #1 - error message

--- interpreter.hs
+++ interpreter.hs
@@ -5,6 +5,14 @@ unitI a = a
 a `bindI` f = f a
 showI = showVal
 
+data E a = Success a | Error String
+unitE a  = Success a
+errorE s = Error s
+(Success a) `bindE` f = f a
+(Error s) `bindE` f = Error s
+showE (Success a) = "Success: " ++ showVal a
+showE (Error s)   = "Error: " ++ s
+
 type Name = String
 data Term = Con Int
     | Var Name
@@ -14,7 +22,7 @@ data Term = Con Int
 
 data Value = Wrong
     | Num Int
-    | Fun (Value -> I Value)
+    | Fun (Value -> E Value)
 
 type Environment = [(Name, Value)]
 
@@ -23,29 +31,33 @@ showVal Wrong = "<wrong>"
 showVal (Fun f) = "<function>"
 showVal (Num i) = show i
 
-interp :: Term -> Environment -> Value
-interp (Con i) e = Num i
+interp :: Term -> Environment -> E Value
+interp (Con i) e = unitE (Num i)
 interp (Var v) e = lookup v e
-interp (Add u v) e = add (interp u e) (interp v e)
-interp (Lam n t) e = Fun (\x -> interp t ((n, x):e))
-interp (App u v) e = apply (interp u e) (interp v e)
+interp (Add u v) e = interp u e `bindE` (\x ->
+                     interp v e `bindE` (\y ->
+                     add x y))
+interp (Lam n t) e = unitE (Fun (\x -> interp t ((n, x):e)))
+interp (App u v) e = interp u e `bindE` (\x ->
+                     interp v e `bindE` (\y ->
+                     apply x y))
 
-lookup :: Name -> Environment -> Value
-lookup n [] = Wrong
+lookup :: Name -> Environment -> E Value
+lookup n [] = unitE Wrong
 lookup n ((a,b):ax)
-    | a == n = b
+    | a == n = unitE b
     | otherwise = lookup n ax
 
-add :: Value -> Value -> Value
-add (Num i) (Num j) = Num (i+j)
-add u v = Wrong
+add :: Value -> Value -> E Value
+add (Num i) (Num j) = unitE (Num (i+j))
+add u v = unitE Wrong
 
-apply :: Value -> Value -> Value
+apply :: Value -> Value -> E Value
 apply (Fun f) v = f v
-apply u v = Wrong
+apply u v = unitE Wrong
 
 test :: Term -> String
-test u = showI $ interp u []
+test u = showE $ interp u []
 
 term0 = (App (Lam "x" (Add (Var "x") (Var "x"))) (Add (Con 11) (Con 10)))
 
