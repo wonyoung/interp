033e0a40dfccd81908fc3b21666a935a441d96bd
Wonyoung <wonyoung.jang@lge.com>
2016-06-14 10:58:25 +0900

    #4 - output

--- interpreter.hs
+++ interpreter.hs
@@ -5,16 +5,27 @@ unitM a = a
 a `bindM` f = f a
 showM = showVal
 
+type O a = (String, a)
+unitO a  = ("", a)
+m `bindO` f = let (r, a) = m
+                  (s, b) = f a
+              in (r ++ s, b)
+showO (s, a) = "Output: " ++ s ++ " Value: " ++ showVal a
+
+outO :: Value -> O ()
+outO a = (showVal a ++ "; ", ())
+
 type Name = String
 data Term = Con Int
     | Var Name
     | Add Term Term
     | Lam Name Term
     | App Term Term
+    | Out Term
 
 data Value = Wrong
     | Num Int
-    | Fun (Value -> M Value)
+    | Fun (Value -> O Value)
 
 type Environment = [(Name, Value)]
 
@@ -23,33 +34,36 @@ showVal Wrong = "<wrong>"
 showVal (Fun f) = "<function>"
 showVal (Num i) = show i
 
-interp :: Term -> Environment -> M Value
-interp (Con i) e = unitM (Num i)
+interp :: Term -> Environment -> O Value
+interp (Con i) e = unitO (Num i)
 interp (Var v) e = lookup v e
-interp (Add u v) e = interp u e `bindM` (\x ->
-                     interp v e `bindM` (\y ->
+interp (Add u v) e = interp u e `bindO` (\x ->
+                     interp v e `bindO` (\y ->
                      add x y))
-interp (Lam n t) e = unitM (Fun (\x -> interp t ((n, x):e)))
-interp (App u v) e = interp u e `bindM` (\x ->
-                     interp v e `bindM` (\y ->
+interp (Lam n t) e = unitO (Fun (\x -> interp t ((n, x):e)))
+interp (App u v) e = interp u e `bindO` (\x ->
+                     interp v e `bindO` (\y ->
                      apply x y))
+interp (Out t) e = interp t e `bindO` (\a ->
+                   outO a `bindO` (\() ->
+                   unitO a))
 
-lookup :: Name -> Environment -> M Value
-lookup n [] = unitM Wrong
+lookup :: Name -> Environment -> O Value
+lookup n [] = unitO Wrong
 lookup n ((a,b):ax)
-    | a == n = unitM b
+    | a == n = unitO b
     | otherwise = lookup n ax
 
-add :: Value -> Value -> M Value
-add (Num i) (Num j) = unitM (Num (i+j))
-add u v = unitM Wrong
+add :: Value -> Value -> O Value
+add (Num i) (Num j) = unitO (Num (i+j))
+add u v = unitO Wrong
 
-apply :: Value -> Value -> M Value
+apply :: Value -> Value -> O Value
 apply (Fun f) v = f v
-apply u v = unitM Wrong
+apply u v = unitO Wrong
 
 test :: Term -> String
-test u = showM $ interp u []
+test u = showO $ interp u []
 
 term0 = (App (Lam "x" (Add (Var "x") (Var "x"))) (Add (Con 11) (Con 10)))
-
+term4 = (Add (Out (Con 41)) (Out (Con 1)))
